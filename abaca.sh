0a1aa4d2e811c6f79136f7d438dd09b09431a161
Youenn Merel <youenn.merel@inria.fr>
2025-03-25 22:07:30 +0100

    Change docker name

--- abaca.sh
+++ abaca.sh
@@ -6,6 +6,8 @@
 #OAR -E ./log/log_%jobid%.stderr
 #OAR -q production
 
+TAG="fmri-confs-runner"
+
 BASE="/home/ymerel/empenn_group_storage/private/ymerel"
 DATA="$BASE/data/auditory"
 RESULTS="$BASE/results/auditory"
@@ -23,7 +25,7 @@ fi
 echo "Running configuration is [$CONF]"
 
 g5k-setup-docker -t
-docker build . -t fmri-fm
-docker run -u root -v "$DATA:/data" -v "$RESULTS:/results" -v "$WORK:/work" -v "$CONFIGS:/data/configs" fmri-fm python run.py --configs "/data/configs/$CONF" --data /data/data_desc.json --ref /data/configs/config_ref.csv
+docker build . -t $TAG
+docker run -u root -v "$DATA:/data" -v "$RESULTS:/results" -v "$WORK:/work" -v "$CONFIGS:/data/configs" $TAG python run.py --configs "/data/configs/$CONF" --data /data/data_desc.json --ref /data/configs/config_ref.csv
 
 
