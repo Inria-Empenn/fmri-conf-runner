b1bb6b51d6bf1021de45e683fe60e995326e8d63
Youenn Merel <youenn.merel@inria.fr>
2025-03-25 17:32:28 +0100

    Add abaca script

---
+++ abaca.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#OAR -l gpu=1,walltime=48:00:00
+#OAR -O ./log_%jobid%.stdout
+#OAR -E ./log_%jobid%.stderr
+#OAR -p abacus19
+#OAR -q production
+
+BASE="/home/ymerel/empenn_group_storage/private/ymerel"
+DATA="$BASE/data/auditory"
+RESULTS="$BASE/results/auditory"
+WORK="$BASE/work"
+CONFIGS="$BASE/configs"
+
+CONF="conf_test.csv"
+
+# Check if a parameter is provided
+if [ -n "$1" ]; then
+  # Override CONF with the provided parameter
+  CONF=$1
+fi
+
+echo "Running configuration is [$CONF]"
+
+g5k-setup-docker -t
+docker build . -t fmri-fm
+docker run -u root -v "$DATA:/data" -v "$RESULTS:/results" -v "$WORK:/work" -v "$CONFIGS:/data/configs" fmri-fm python run.py --configs "/data/configs/$CONF" --data /data/data_desc.json --ref /data/configs/config_ref.csv
+
+
